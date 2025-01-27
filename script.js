76f43d864ff427df3c3a36273a1b92bded031b0f
Sydney J <sjennings16@my.bcit.ca>
2025-01-27 10:39:36 -0800

    created index.html and script.js for JSChallenges 1

---
+++ script.js
@@ -0,0 +1,19 @@
+//Function to output a console message and create an alert popup.
+function sayHello() {
+    console.log("hello world!");
+    alert("hello world!");
+}
+
+// Call the function sayHello()
+sayHello();
+
+//A function called `sayHello()` is defined.  It has two statements:
+
+// - `console.log` (to output a string to the console of the Browser Inspector) (Open with F12 on most browsers)
+// - `alert()`  to open a popup with a string
+
+// After a function is defined, it must be called (or “invoked”) in order to be executed.
+
+// We can do with one simple line of JavaScript that contains the function name. 
+
+// A function can also be called from the F12 inspector console window.
